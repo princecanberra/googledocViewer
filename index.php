5c8f28a784ff790d6f9ef29c51e312cc7261b7ce
AP286 <ouphachay@gmail.com>
2013-05-29 18:13:39 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+		<title>Test google viewer</title>
+	
+	</head>
+
+	<body>
+	<?php $url="www.acm.org/sigs/publications/pubform.doc";  ?>
+	
+	  <iframe src="http://docs.google.com/gview?url=<?php echo $url; ?>&embedded=true" style="width:850px; height:500px;" frameborder="0"></iframe>
+	
+	<br/>
+	<br/><a href="www.acm.org/sigs/publications/pubform.doc">Click here to download</a>
+	
+	</body>
+
+</html>
